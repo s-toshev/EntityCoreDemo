4c629cb7f7b58d8e8c532a4424a06db8e2591f21
s-toshev <simeon.toshev@yahoo.bg>
2024-03-04 21:01:15 +0200

    4 Mar

--- EntityCoreDemo/StartUp.cs
+++ EntityCoreDemo/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
 using PersonRegister.Data;
+using PersonRegister.Data.Models;
 
 namespace PersonRegister
 {
@@ -14,10 +15,21 @@ namespace PersonRegister
             var dbContext = contextFactory.CreateDbContext(args);
 
             await dbContext.Database.MigrateAsync();
+            //Person person = new Person();
+            //person.FirstName = "zonzo";
+            //person.LastName = "the";
+            //person.City = "mix";
+            //person.Age = 33;
 
+            // dbContext.Person.Add(person);
+            //await dbContext.SaveChangesAsync();
 
+            var person = dbContext.Person.Where(p => p.FirstName == "zonzo").ToList();
 
-
+            foreach (var p in person)
+            {
+                await Console.Out.WriteLineAsync($"{p.FirstName} is from {p.LastName} {p.City}");
+            }
         }
 
 
